5fac4530fc3e546a16a8527a2c8cb02f473b7b67
ahmad shadcome <a.shadcome@gmail.com>
2021-11-26 19:58:21 +0330

    Completed footer

--- src/app/modules/main-layout/main-layout.module.ts
+++ src/app/modules/main-layout/main-layout.module.ts
@@ -9,6 +9,8 @@ import { TopbarComponent, MenuComponent, BreadcrumbComponent, DownloadComponent,
 /// Material Modules
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule, MAT_FORM_FIELD_DEFAULT_OPTIONS } from '@angular/material/form-field';
 
 @NgModule({
   declarations: [
@@ -25,7 +27,12 @@ import { MatButtonModule } from '@angular/material/button';
     CommonModule,
     MainLayoutRoutingModule,
     MatIconModule,
-    MatButtonModule
+    MatButtonModule,
+    MatInputModule,
+    MatFormFieldModule
+  ],
+  providers: [
+    {provide: MAT_FORM_FIELD_DEFAULT_OPTIONS, useValue: {floatLabel: 'always'}}
   ]
 })
 export class MainLayoutModule { }
