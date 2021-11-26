a0886cc4eda6d4bde852ed2bedb7a657696f6cab
ahmad shadcome <a.shadcome@gmail.com>
2021-11-26 09:28:48 +0330

    Completed menubar

--- src/app/modules/main-layout/components/menu/menu.component.ts
+++ src/app/modules/main-layout/components/menu/menu.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { IMenu } from 'src/app/interfaces';
 
 @Component({
   selector: 'app-menu',
@@ -7,9 +8,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MenuComponent implements OnInit {
 
-  constructor() { }
+  menu: IMenu[] = [];
+
+  constructor() {
+    this.menu = [
+      { title: 'Home', route: '', isActive: true },
+      { title: 'My Courses', route: 'my-courses', isActive: false },
+      { title: 'Pages', route: '', isActive: false },
+      { title: 'Blog', route: '', isActive: false },
+      { title: 'Contacts', route: '', isActive: false }
+    ]
+  }
 
   ngOnInit(): void {
+
   }
 
 }
