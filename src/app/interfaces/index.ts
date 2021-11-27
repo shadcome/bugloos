6bf52c5ea20be1fe93b9c30ee14833a2b3cc67c2
ahmad shadcome <a.shadcome@gmail.com>
2021-11-27 22:49:17 +0330

    Completed Register

--- src/app/interfaces/index.ts
+++ src/app/interfaces/index.ts
@@ -3,3 +3,4 @@
  */
 
 export * from './menu.interface'
+export * from './user-info.interface'
---
+++ src/app/interfaces/user-info.interface.ts
@@ -0,0 +1,6 @@
+export interface IUserInfo {
+    name: string;
+    email: string;
+    photo: any | null;
+    gender: number;
+}
