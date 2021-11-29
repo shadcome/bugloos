f462c58bb65ff9e5f01f179b16270a8bc18d1558
ahmad shadcome <a.shadcome@gmail.com>
2021-11-29 12:37:50 +0330

    Completed guard

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
 import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
 
@@ -7,19 +8,20 @@ import { Observable } from 'rxjs';
 })
 export class AuthGuard implements CanActivate {
 
-  constructor(private route: Router) { }
+  constructor(private route: Router, private srvSnack: MatSnackBar) { }
 
   /**
- * This guard checks the token stored in the Local Storage and allows entry if it exists.
+ * This guard checks the user stored in the Local Storage and allows entry if it exists.
  */
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
     return new Observable<boolean>(obs => {
-      const token = localStorage.getItem('token');
-      if (!!token) {
+      const user = localStorage.getItem('user');
+      if (!!user) {
         obs.next(true);
       } else {
+        this.srvSnack.open('Please register first .', 'Ok')
         this.route.navigate(['/auth']);
         obs.next(false);
       }
--- src/app/modules/courses/courses-routing.module.ts
+++ src/app/modules/courses/courses-routing.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from 'src/app/guards';
 import { CourseListComponent } from './components';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'all' },
   { path: 'all', component: CourseListComponent },
-  { path: 'user-courses', component: CourseListComponent }
+  { path: 'user-courses', canActivate: [AuthGuard], component: CourseListComponent }
 ];
 
 @NgModule({
